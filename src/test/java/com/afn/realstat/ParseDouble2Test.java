93df27d07aa48df6add297479d6c25b29de6e0f0
afneyer <afneyer@gmail.com>
2017-01-17 13:52:36 -0800

    new Parsedate2 processor new ParseDouble2 test

---
+++ src/test/java/com/afn/realstat/ParseDouble2Test.java
@@ -0,0 +1,22 @@
+package com.afn.realstat;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.supercsv.cellprocessor.ParseDouble2;
+
+public class ParseDouble2Test {
+
+
+	@Test
+	public void testTranslateCsvFileDefault() {
+		
+		ParseDouble2 parser = new ParseDouble2();
+
+		assertEquals("2.0", parser.cleanStrDouble2("2.0"));
+		assertEquals(null, parser.cleanStrDouble2("MONTCL"));
+		assertEquals("3", parser.cleanStrDouble2("*3"));
+		
+	}
+
+}
