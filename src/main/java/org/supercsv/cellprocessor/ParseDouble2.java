824c369aad543cc60106658bf2ee93da57971942
afneyer <afneyer@gmail.com>
2017-01-17 13:51:35 -0800

    Remove more characters before parsing

--- src/main/java/org/supercsv/cellprocessor/ParseDouble2.java
+++ src/main/java/org/supercsv/cellprocessor/ParseDouble2.java
@@ -1,53 +1,66 @@
 package org.supercsv.cellprocessor;
 
+import org.supercsv.cellprocessor.CellProcessorAdaptor;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.exception.SuperCsvCellProcessorException;
+import org.supercsv.util.CsvContext;
 
+/**
+ * Custom cell processor that removes commas and $signs before calling the
+ * standard ParseDouble CellProcessor
+ * 
+ * e.g. 100,000 -> 100000 $200,000 - 200000
+ */
+public class ParseDouble2 extends CellProcessorAdaptor {
 
-	import org.supercsv.cellprocessor.CellProcessorAdaptor;
-	import org.supercsv.cellprocessor.ift.CellProcessor;
-	import org.supercsv.exception.SuperCsvCellProcessorException;
-	import org.supercsv.util.CsvContext;
-
-	/**
-	 * Custom cell processor that removes commas and $signs before calling
-	 * the standard ParseDouble CellProcessor
-	 * 
-	 * e.g. 100,000 -> 100000
-	 * 		$200,000 - 200000
-	 */
-	public class ParseDouble2 extends CellProcessorAdaptor {
-	        
-	        public ParseDouble2() {
-	                super();
-	        }
-	        
-	        public ParseDouble2(CellProcessor next) {
-	                // this constructor allows other processors to be chained after ParseDouble2
-	                super(next);
-	        }
-	        
-	        public Object execute(Object value, CsvContext context) {
-	                
-	                validateInputNotNull(value, context);  // throws an Exception if the input is null
-	                
-	                // remove commas
-	                String newValue = new String( value.toString() );
-	                newValue = newValue.replaceAll(",","");
-	                newValue = newValue.replaceAll("\\$","");
-	                newValue = newValue.replaceAll(" ","");
-	                newValue = newValue.replaceAll("\\%","");
-	                
-	                // call standard cell parseDouble() CellProcessor
-	                ParseDouble parseDouble = new ParseDouble();
-	                
-	                Object obj;
-					try {
-						obj = parseDouble.execute(newValue, context);
-					} catch (Exception e) {
-						throw new SuperCsvCellProcessorException(
-		                        String.format("Could not parse '%s' as a Double2", value), context, this);
-					}
-	                return obj;
-	        }
+	public ParseDouble2() {
+		super();
 	}
-	
 
+	public ParseDouble2(CellProcessor next) {
+		// this constructor allows other processors to be chained after
+		// ParseDouble2
+		super(next);
+	}
+
+	public Object execute(Object value, CsvContext context) {
+
+		validateInputNotNull(value, context); // throws an Exception if the
+												// input is null
+
+		// clean the string
+		String newValue = cleanStrDouble2(value.toString());
+		
+		// This parser processes optional values since the cleaning process may result in null 
+		if (newValue == null) {
+			return null;
+		}
+
+		// call standard cell parseDouble() CellProcessor
+		ParseDouble parseDouble = new ParseDouble();
+
+		Object obj;
+		try {
+			obj = parseDouble.execute(newValue, context);
+		} catch (Exception e) {
+			throw new SuperCsvCellProcessorException(String.format("Could not parse '%s' as a Double2", newValue),
+					context, this);
+		}
+		return obj;
+	}
+
+	public String cleanStrDouble2(String inStr) {
+		// remove commas
+		String outStr = new String(inStr.toString());
+		outStr = outStr.replaceAll(",", "");
+		outStr = outStr.replaceAll("\\$", "");
+		outStr = outStr.replaceAll(" ", "");
+		outStr = outStr.replaceAll("\\%", "");
+		outStr = outStr.replaceAll("[A-Z]", "");
+		outStr = outStr.replaceAll("\\*", "");
+		if (outStr.isEmpty()) {
+			outStr = null;
+		}
+		return outStr;
+	}
+}
