585b94cb593f9b23b1f6de6923d4ac46926b0062
afneyer <afneyer@gmail.com>
2017-01-17 13:24:36 -0800

    Expanded tests for CsvFieldTranslater

--- src/test/java/com/afn/realstat/AbstractImporterTest.java
+++ src/test/java/com/afn/realstat/AbstractImporterTest.java
@@ -6,32 +6,36 @@ import org.junit.Test;
 
 // @RunWith(SpringRunner.class)
 // @SpringBootTest(classes = Application.class,
- //        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//        webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class AbstractImporterTest {
-	
-	 // @Autowired TODO
-	 // private RealPropertyRepository repository;
-	 
-	 // @Autowired TODO
-	 // private CrsImporter importer;
 
-	 @Test
-	 public void testTranslateCsvFileDefault() {
-		
-	    assertEquals("apn", CrsImporter.translateCsvFileFieldDefault("APN"));
-	    assertEquals("owner1",CrsImporter.translateCsvFileFieldDefault("Owner 1"));
-	    assertEquals("firstName",CrsImporter.translateCsvFileFieldDefault("First Name"));
-	    assertEquals("firstName2",CrsImporter.translateCsvFileFieldDefault("First Name2"));
-	    assertEquals("totalSquareFootage",CrsImporter.translateCsvFileFieldDefault("Total Square Footage"));
-	    assertEquals("lotSqFeet", CrsImporter.translateCsvFileFieldDefault("Lot Sq. Feet"));
-	    assertEquals("listPriceSqft",CrsImporter.translateCsvFileFieldDefault("List $/Sqft"));
-	    assertEquals("occPercent",CrsImporter.translateCsvFileFieldDefault("Occ%"));
-	    assertEquals("pool",CrsImporter.translateCsvFileFieldDefault("Pool (Y/N)"));
-	    assertEquals("priceSqft",CrsImporter.translateCsvFileFieldDefault("Price/SqFt"));
-	    assertEquals("salePriceSqft",CrsImporter.translateCsvFileFieldDefault("Sale $/SqFt"));
-	    assertEquals("saleLastListPrice",CrsImporter.translateCsvFileFieldDefault("Sale/Last List $"));
-	    assertEquals("ticPercentOwnerOffered",CrsImporter.translateCsvFileFieldDefault("TIC % Owner Offered"));
+	// @Autowired TODO
+	// private RealPropertyRepository repository;
+
+	// @Autowired TODO
+	// private CrsImporter importer;
+
+	@Test
+	public void testTranslateCsvFileDefault() {
+
+		assertEquals("apn", CrsImporter.translateCsvFileFieldDefault("APN"));
+		assertEquals("owner1", CrsImporter.translateCsvFileFieldDefault("Owner 1"));
+		assertEquals("firstName", CrsImporter.translateCsvFileFieldDefault("First Name"));
+		assertEquals("firstName2", CrsImporter.translateCsvFileFieldDefault("First Name2"));
+		assertEquals("totalSquareFootage", CrsImporter.translateCsvFileFieldDefault("Total Square Footage"));
+		assertEquals("lotSqFeet", CrsImporter.translateCsvFileFieldDefault("Lot Sq. Feet"));
+		assertEquals("listPriceSqft", CrsImporter.translateCsvFileFieldDefault("List $/Sqft"));
+		assertEquals("occPercent", CrsImporter.translateCsvFileFieldDefault("Occ%"));
+		assertEquals("pool", CrsImporter.translateCsvFileFieldDefault("Pool (Y/N)"));
+		assertEquals("priceSqft", CrsImporter.translateCsvFileFieldDefault("Price/SqFt"));
+		assertEquals("salePriceSqft", CrsImporter.translateCsvFileFieldDefault("Sale $/SqFt"));
+		assertEquals("saleLastListPrice", CrsImporter.translateCsvFileFieldDefault("Sale/Last List $"));
+		assertEquals("ticPercentOwnerOffered", CrsImporter.translateCsvFileFieldDefault("TIC % Owner Offered"));
+		assertEquals("coListAgentLicenseId", CrsImporter.translateCsvFileFieldDefault("Co-List Agent - License ID"));
+		assertEquals("coSellAgentLicenseId", CrsImporter.translateCsvFileFieldDefault("Co-Sell Agent - License ID"));
+		assertEquals("listingAgentLicenseId", CrsImporter.translateCsvFileFieldDefault("Listing Agent - License ID"));
+		assertEquals("sellingAgent1LicenseId", CrsImporter.translateCsvFileFieldDefault("Selling Agent 1 - License ID"));
 
 	}
-	 
+
 }
