a2316692a74a71ae3c1b8cd99863679b8489338e
afneyer <afneyer@gmail.com>
2017-01-28 13:38:19 -0800

    complete PersName class used to parse personal names including tests

---
+++ src/main/java/com/afn/realstat/PersName.java
@@ -0,0 +1,180 @@
+package com.afn.realstat;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import javatools.parsers.Name.PersonName;
+
+public class PersName {
+
+	private static final Logger log = (Logger) LoggerFactory.getLogger("import");
+
+	private String rawName;
+	private String normalizedName;
+	private String firstName;
+	private String middleName;
+	private String middleInitial;
+	private String lastName;
+
+	public PersName() {
+	};
+
+	// The raw name is cleaned up by removing multiple blanks, trimming front
+	// and back and capitalizing it.
+	public PersName(String rawName) {
+		this.rawName = rawName;
+	    normalizedName = StringUtils.trim(rawName).replaceAll(" +", " ");
+	    normalizedName = WordUtils.capitalizeFully((this.normalizedName).toLowerCase());
+		normalizedName = normalizeName(this.normalizedName);
+		PersonName pn = new PersonName(this.normalizedName);
+		lastName = pn.familyName();
+		extractFirstMiddle(pn.givenNames());
+
+	}
+
+	private String normalizeName(String raw) {
+		String[] nameParts = raw.split(",");
+
+		switch (nameParts.length) {
+		case 1:
+			return raw;
+		case 2:
+			String lastName = StringUtils.trim(nameParts[0]);
+			String firstMiddle = StringUtils.trim(nameParts[1]);
+			firstMiddle = firstMiddle + " " + lastName;
+			return firstMiddle;
+		default:
+			log.warn("Name contains multiple commas, may not be able to parse it!" + rawName);
+			return raw;
+		}
+	}
+
+	private void extractFirstMiddle(String firstMiddle) {
+		// split by blank
+		firstMiddle = firstMiddle.replaceAll("  ", " ");
+		firstMiddle = StringUtils.trim(firstMiddle);
+		String[] nameParts2 = firstMiddle.split(" ");
+
+		switch (nameParts2.length) {
+		case 1:
+			firstName = StringUtils.trim(nameParts2[0]);
+			middleName = null;
+			middleInitial = null;
+			break;
+		case 2:
+			firstName = StringUtils.trim(nameParts2[0]);
+			middleName = StringUtils.trim(nameParts2[1]);
+			cleanMiddleName();
+			extractMiddleInitial();
+			break;
+		default:
+			log.warn("Unparseable Agent Name " + rawName);
+			break;
+		}
+	}
+
+	// this is not needed anymore TODO
+	@SuppressWarnings("unused")
+	private void extractFirstMiddleLast() {
+
+		// split by comma
+		String[] nameParts = rawName.split(",");
+		// TODO process non-comma formats
+		if (nameParts.length == 2) {
+
+			lastName = WordUtils.capitalizeFully(StringUtils.trim(nameParts[0]).toLowerCase());
+
+			// split by blank
+			String firstMiddle = nameParts[1];
+			firstMiddle = firstMiddle.replaceAll("  ", " ");
+			firstMiddle = StringUtils.trim(firstMiddle);
+			String[] nameParts2 = firstMiddle.split(" ");
+
+			switch (nameParts2.length) {
+			case 1:
+				firstName = StringUtils.trim(nameParts2[0]);
+				middleName = null;
+				middleInitial = null;
+				break;
+			case 2:
+				firstName = StringUtils.trim(nameParts2[0]);
+				middleName = StringUtils.trim(nameParts2[1]);
+				cleanMiddleName();
+				extractMiddleInitial();
+				break;
+			default:
+				log.warn("Unparseable Agent Name " + rawName);
+				break;
+			}
+		}
+
+	}
+
+	private void cleanMiddleName() {
+		middleName = middleName.replaceAll("[.]", "");
+
+	}
+
+	private void extractMiddleInitial() {
+
+		if (middleName == null || middleName.isEmpty()) {
+			middleInitial = null;
+			return;
+		}
+		if (middleName.length() >= 1) {
+			middleInitial = middleName.substring(0, 1);
+			return;
+		}
+	}
+
+	public String getRawName() {
+		return rawName;
+	}
+
+	public void setRawName(String rawName) {
+		this.rawName = rawName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getNormalizedName() {
+		return normalizedName;
+	}
+
+	public void setNormalizedName(String normalizedName) {
+		this.normalizedName = normalizedName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
---
+++ src/test/java/com/afn/realstat/PersNameTest.java
@@ -0,0 +1,120 @@
+package com.afn.realstat;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import org.junit.Test;
+
+import javatools.parsers.NameML;
+
+import javax.naming.NameParser;
+
+public class PersNameTest {
+
+	@Test
+	public void nameLibraryTest() {
+		
+		javatools.parsers.Name.PersonName n;
+		
+		n = new javatools.parsers.Name.PersonName("Sergio Vieira de Mello");
+		assertEquals("Sergio Vieira Mello", n.normalize());
+		assertEquals("Mello", n.familyName());
+		assertEquals("Sergio Vieira", n.givenNames());
+		assertEquals("Sergio", n.givenName());
+		
+		n = new javatools.parsers.Name.PersonName("Andreas Neyer");
+		assertEquals("Andreas Neyer", n.normalize());
+		assertEquals("Neyer", n.familyName());
+		assertEquals("Andreas", n.givenNames());
+		assertEquals("Andreas", n.givenName());
+		
+		n = new javatools.parsers.Name.PersonName("Andreas F. Neyer");
+		assertEquals("Andreas F. Neyer", n.normalize());
+		assertEquals("Neyer", n.familyName());
+		assertEquals("Andreas F.", n.givenNames());
+		assertEquals("Andreas", n.givenName());
+		
+		n = new javatools.parsers.Name.PersonName("Andreas F Neyer");
+		assertEquals("Andreas F Neyer", n.normalize());
+		assertEquals("Neyer", n.familyName());
+		assertEquals("Andreas F", n.givenNames());
+		assertEquals("Andreas", n.givenName());
+		
+		n = new javatools.parsers.Name.PersonName("Andreas Felix Neyer");
+		assertEquals("Andreas Felix Neyer", n.normalize());
+		assertEquals("Neyer", n.familyName());
+		assertEquals("Andreas Felix", n.givenNames());
+		assertEquals("Andreas", n.givenName());	
+	}
+	
+	@Test
+	public void testConstructor() {
+
+		PersName n;
+		
+		n = new PersName("Neyer, Andreas");
+		assertEquals("Andreas Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals(null, n.getMiddleName());
+		assertEquals(null, n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+		n = new PersName("Andreas F. Neyer");
+		assertEquals("Andreas F. Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("F", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+
+		n = new PersName("Andreas F. Neyer");
+		assertEquals("Andreas F. Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("F", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+		n = new PersName("Neyer, Andreas Felix");
+		assertEquals("Andreas Felix Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("Felix", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+		n = new PersName("Andreas Felix Neyer");
+		assertEquals("Andreas Felix Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("Felix", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+		n = new PersName("NEYER, Andreas    F");
+		assertEquals("Andreas F Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("F", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+		n = new PersName("   ANDreas f neyer    ");
+		assertEquals("Andreas F Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("F", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+		n = new PersName(" NEYER, ANDREAS    FELIX");
+		assertEquals("Andreas Felix Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("Felix", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+
+		n = new PersName("    Neyer, Andreas fELIX      ");
+		assertEquals("Andreas Felix Neyer", n.getNormalizedName());
+		assertEquals("Andreas", n.getFirstName());
+		assertEquals("Felix", n.getMiddleName());
+		assertEquals("F", n.getMiddleInitial());
+		assertEquals("Neyer", n.getLastName());
+	}
+
+}
