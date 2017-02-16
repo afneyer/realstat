26ebdbc98c7d8e7ffff2b463e8f4af24cf2761e3
afneyer <afneyer@gmail.com>
2017-02-16 12:19:19 -0800

    updated sample scripts

--- sqlscript/dbScriptSamples.sql
+++ sqlscript/dbScriptSamples.sql
@@ -13,14 +13,20 @@ addressClean in
 ( select addressClean from real_property group by addressClean having count(addressClean)>1 );
 */
 /*
-select count(*) from property_transaction where realProperty_id is not null;
+select count(id) as totalCnt, count(realProperty_id) as linkedCnt, 
+100*count(realProperty_id)/count(id) as linkedPercent, substring(zip,1,5) as zipCode, city 
+from property_transaction where substring(zip,1,5) in ('94610', '94611', '94618') 
+group by zipCode, city
+having count(id) > 3;
+*/
+
+/*
 select count(*) from property_transaction;
 select count(*) from real_property;*/
 
 /*
 drop table agent;
 */
-
 /*
 select status,  
 count(ListingAgentName), count(SellingAgent1Name), count(CoListAgentName), count(CoSellAgentName), 
@@ -29,20 +35,45 @@ count(ListAgentBreNum), count(SoldAgentBreNum), count(CoListAgentBreNum), count(
 count(listingAgent_id), count(sellingAgent_id), count(listingAgent2_id), count(sellingAgent2_id)
 from property_transaction group by status;
 */
-
-
+/*
+select substr(zip,1,5) as zipCode,
+count(ListingAgentName) as list1NameCnt,
+100 * count(listingAgent_id) / count(ListingAgentName) as list1Percent,
+count(SellingAgent1Name) as sell1NameCnt,
+100 * count(sellingAgent_id) / count(SellingAgent1Name) as sell1Percent,
+count(CoListAgentName) as list2NameCnt,
+100 * count(listingAgent2_id) / count(CoListAgentName) as list2Percent,
+count(CoSellAgentName) as sell2NameCnt,
+100 * count(sellingAgent2_id) / count(CoSellAgentName) as sell2Percent
+from property_transaction 
+where substr(status,1,3) = 'SLD' and substr(zip,1,5) in ('94610','94611','94618')
+group by substr(zip,1,5);
+*/
+/*
 select buildingType, status, 
-count(realProperty_id), count(id), (count(id) - count(realProperty_id)) /count(id)
+count(realProperty_id), count(id), (count(id) - count(realProperty_id))/count(id)
 from property_transaction
 where zip like "94611%"
 group by  buildingType, status;
-
-
-/*
-select landUse,
-count(id), count(id)
-from real_property group by landUse;
 */
+
+
+/*select buildingType, status, landUse, substr(zip,1,5) as zipCode,
+count(realProperty_id), count(pt.id) 
+from property_transaction pt, real_property rp
+where pt.realProperty_id = rp.id and substr(status,1,3) = 'SLD' 
+group by  buildingType, status, landUse, zipCode;*/
+
+
+select count(id), substr(propertyZip,1,5), propertyCity, landUse
+from real_property
+where landUse in ('Single Family Residential', 'Residential Condominium', 'Residential Townhouse') 
+group by substr(propertyZip,1,5), propertyCity, landUse;
+
+select count(pt.id), substr(zip,1,5),  city, buildingType, year(closeDate) 
+from property_transaction pt, real_property rp
+where pt.realProperty_id = rp.id and substr(status,1,3) = 'SLD' and substr(zip,1,5) in ('94610','94611', '94618') 
+group by  substr(zip,1,5),  city, buildingType, buildingType, year(closeDate);
 
 /*
 select 
@@ -108,17 +139,38 @@ drop table agent_volume;
 drop table property_transaction;
 */
 
-/*
-select count(status),status from property_transaction group by status;
-select count(*) from property_transaction;
-*/
 
 
 /*
 select lastSaleDate from real_property where lastSaleDate is not null;
 */
+/*
+select year(closeDate) as closeYear, city, substring(zip,1,5) as zipCode, count(id) 
+from property_transaction
+where closeDate is not null
+group by closeYear, city, zipCode;
+*/
+
+/*
+select sum(case when(pt.listingAgent_id = pt.sellingAgent_id) then 1 else 0 end) as doubleEnded, 
+(100*sum(case when(pt.listingAgent_id = pt.sellingAgent_id) then 1 else 0 end)/count(pt.id)) as percentDoubleEnded, count(pt.id) as allTrans, a.agentName 
+from property_transaction pt, agent a
+where pt.sellingAgent_id = a.id or pt.listingAgent_id = a.id 
+group by a.agentName
+having count(pt.id) > 20
+order by percentDoubleEnded desc;
+*/
 
 
+
+/*select count(*) from property_transaction;
+*/
+
+/*delete from property_transaction;*/
+
+/*
+* Relative sales activity by year
+*/
 
 
 
