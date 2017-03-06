a5c4754513d9574d1a230aae6a7a1d578af1bff7
afneyer <afneyer@gmail.com>
2017-03-06 15:43:13 -0800

    Probably old and obsolete class, eventually remove

--- src/main/java/com/afn/realstat/TransactionManagerConfig.java
+++ src/main/java/com/afn/realstat/TransactionManagerConfig.java
@@ -1,15 +1,8 @@
 package com.afn.realstat;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -18,12 +11,18 @@ class TransactionManagerConfig {
 
 	public static final Logger log = LoggerFactory.getLogger("app");
 
+	// TODO remove
+	/*
 	@Autowired
 	EntityManagerFactory emf;
+	*/
 	
+	/*
 	@Autowired
 	private DataSource afn1DataSource;
-
+	*/
+	
+	/*
 	@Bean(name = "transactionManager")
 	public PlatformTransactionManager transactionManager() {
 		JpaTransactionManager tm = new JpaTransactionManager();
@@ -31,5 +30,6 @@ class TransactionManagerConfig {
 		tm.setDataSource(afn1DataSource);
 		return tm;
 	}
+	*/
 
 }
